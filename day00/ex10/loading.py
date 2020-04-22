db3b2702fdd038cbdc6e1804b14ef2e6f4803144
Eduardo Ramirez <edramir18@gmail.com>
2020-04-22 10:10:49 +0200

    Terminada loading bar

--- day00/ex10/loading.py
+++ day00/ex10/loading.py
@@ -1,23 +1,20 @@
-import sys
-
-
 def ft_progress(lst):
     size = len(lst)
     eta = size * 0.01
-    size_len = len(str(size))
-    for c, i in enumerate(lst):
-        progress = f'{"":#>{(c + 1) * 100 // size // 4}}'
-        print(f'\rETA {eta:4.2f}s [{progress:25}] {c + 1:{size_len}}/{size:{size_len}}'
-              f' | elapsed time {c * 0.01:4.2f}s', end='', flush=True)
-        yield i
+    for k, v in enumerate(lst, start=1):
+        pct = (k * 20) // size
+        progress = f'{"":#>{pct}}'
+        print(f'\rETA {eta:4.2f}s [{progress:20}] {k:5}/{size:<5}'
+              f'| elapsed time {k * 0.01:4.2f}s', end='', flush=True)
+        yield v
 
 
 if __name__ == '__main__':
     import time
     import random
 
-    a = random.randint(1, 100)
-    listy = range(a * 100)
+    a = random.randint(500, 1000)
+    listy = range(a)
     ret = 0
     for element in ft_progress(listy):
         ret += (element + 3) % 5
